4648b9438d31dc41dddb434e50610d4e20e79222
Oskar Ochal <spacespiderproductions@gmail.com>
2024-06-22 21:58:36 +0200

    Initial commit

---
+++ src/main/java/com/space/travellerserver/TravellerServerApplication.java
@@ -0,0 +1,13 @@
+package com.space.travellerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space/travellerserver/TravellerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.travellerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
